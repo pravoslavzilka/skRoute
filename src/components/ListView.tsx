744f3a7c12bcef4e7028d35d5190bfd0a07e100e
Anna Mertušová <pravoslav.zilka@gmail.com>
2025-02-15 21:14:58 +0100

    Backend connected

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,9 +1,30 @@
 import './comp.css'
 import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 
 
 function ListView () {
+
+
+	
+	const [message, setMessage] = useState('waiting');
+
+
+  	useEffect(() => {
+
+
+    fetch("http://127.0.0.1:8000/", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+        console.log(data.message);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
 
 	return (
 
@@ -13,6 +34,8 @@ function ListView () {
 		<h1 className="text-3xl font-bold underline text-red-600">Hello l</h1>
 		<h1 className="text-3xl font-bold underline text-red-600"><NavLink to="/">Hello l</NavLink></h1>
 
+		<p>{message}</p>
+				
 		</>
 
 
--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -9,7 +9,7 @@ function NoMatch () {
 
 			<div>
 			<h1>404</h1>
-			<h4>No Match</h4>
+			<h4>Nenájdené</h4>
 
 			</div>
 
